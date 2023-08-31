056bb6317aa6159ca4ad099923719b6254a51f34
ahn_youngseon <ys_ahn@sk.com>
2023-08-31 20:00:15 +0900

    reformat

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
-import { OrbitControls, Stage, useGLTF } from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
+import {Suspense} from "react";
+import {OrbitControls, Stage, useGLTF} from "@react-three/drei";
 import Model from "./app/components/Model.tsx";
 
 useGLTF.preload('/robot.gltf')
 
 function App() {
   return (
-    <Canvas shadows camera={{ fov: 45, aspect: window.innerWidth / window.innerHeight, near: 0.25, far: 100 }}>
+    <Canvas shadows camera={{
+      fov: 45,
+      aspect: window.innerWidth / window.innerHeight,
+      near: 0.25,
+      far: 100,
+      position: [-5, 3, 10]
+    }}>
       <Suspense fallback={null}>
-        <Stage shadows={{ type: 'contact', opacity: 1, blur: 2, size: 1}} >
+        <Stage shadows={{type: 'contact', opacity: 1, blur: 2, size: 1}}>
           <mesh>
-            <hemisphereLight intensity={0.15} groundColor="black" />
-            <Model scale={[0.01,0.01,0.01]} />
+            <hemisphereLight intensity={0.15} groundColor="black"/>
+            <Model scale={[0.01, 0.01, 0.01]}/>
           </mesh>
         </Stage>
       </Suspense>
-      <OrbitControls makeDefault />
+      <OrbitControls makeDefault/>
     </Canvas>
   )
 }
