b96600702242f8b040f1720df60035b608465cb2
ahn_youngseon <ys_ahn@sk.com>
2023-08-31 20:19:11 +0900

    미사용 코드 삭제

--- src/app/components/Model.tsx
+++ src/app/components/Model.tsx
@@ -2,14 +2,13 @@ import {useAnimations, useGLTF} from "@react-three/drei";
 import {useEffect} from "react";
 
 function Model(props: any) {
-  const { scene, animations } = useGLTF('/robot-draco.glb')
+  const { scene, animations } = useGLTF('/RobotExpressive.glb')
   const { actions } = useAnimations(animations, scene)
 
   useEffect(() => {
     if (!actions.Idle) return;
 
-    actions.Idle.play()
-    scene.traverse((obj) => obj.isMesh && (obj.receiveShadow = obj.castShadow = true))
+    actions.Idle.play();
   }, [actions, scene])
   return <primitive object={scene} {...props} />
 }
